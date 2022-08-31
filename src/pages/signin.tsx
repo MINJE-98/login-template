9cc8df0a973c8777f1a30b95e542cde68c18007f
MINJE-98 <jmj012100@gmail.com>
2022-08-31 22:55:59 +0900

    feat(pages): 로그인 Default pages 추가

---
+++ src/pages/signin.tsx
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+import Router from 'next/router';
+import Layout from '@Components/layout';
+import Form from '@Components/form';
+
+const Login = () => {
+  const [errorMsg, setErrorMsg] = useState('');
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    if (errorMsg) setErrorMsg('');
+
+    const body = {
+      username: e.currentTarget.username.value,
+      password: e.currentTarget.password.value,
+    };
+
+    try {
+      const res = await fetch('/api/signin', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(body),
+      });
+      if (res.status === 200) {
+        Router.push('/');
+      } else {
+        throw new Error(await res.text());
+      }
+    } catch (error) {
+      setErrorMsg('유저 이름 또는 비밀번호가 잘못됐습니다.');
+    }
+  }
+
+  return (
+    <Layout>
+      <div className="login">
+        <Form isLogin errorMessage={errorMsg} onSubmit={handleSubmit} />
+      </div>
+      <style jsx>{`
+        .login {
+          max-width: 21rem;
+          margin: 0 auto;
+          padding: 1rem;
+          border: 1px solid #ccc;
+          border-radius: 4px;
+        }
+      `}</style>
+    </Layout>
+  );
+};
+
+export default Login;
