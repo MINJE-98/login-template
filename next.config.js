60398b75110c3bd9e3a39a9e342d612ca4470939
MINJE-98 <jmj012100@gmail.com>
2022-09-01 19:29:38 +0900

    fix: env cinfig

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
-
+console.log('load env =>', process.env);
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  env: {
+    SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL,
+  },
 };
 
 module.exports = nextConfig;
