dd57d20dbf01240846fb9e90f2d58a875a3786ed
MINJE-98 <jmj012100@gmail.com>
2022-09-01 15:11:14 +0900

    feat(_app): provider 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,18 @@ import '../../styles/globals.css';
 import type { AppProps } from 'next/app';
 
 import { QueryClient, QueryClientProvider } from 'react-query';
+import AuthProvider from 'src/hooks/auth/AuthProvider';
 
 const queryClient = new QueryClient({});
-const MyApp = ({ Component, pageProps }: AppProps) => {
+const MyApp = ({
+  Component,
+  pageProps: { initUserInfo, ...pageProps },
+}: AppProps) => {
   return (
     <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
+      <AuthProvider initUserInfo={initUserInfo}>
+        <Component {...pageProps} />
+      </AuthProvider>
     </QueryClientProvider>
   );
 };
