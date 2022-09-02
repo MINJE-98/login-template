ce5801c3293fd7769b0f40056b372928e8b778e2
MINJE-98 <jmj012100@gmail.com>
2022-09-02 21:04:18 +0900

    feat(passport): google strategy 적용

--- lib/passport/index.ts
+++ lib/passport/index.ts
@@ -5,6 +5,7 @@ import passport from 'passport';
 import { findUserWithUserId } from '@Lib/db/findUser';
 import { getMongoDb } from '@Lib/db/mongoClient';
 import githubStrategy from '@Lib/passport/strategy/githubStrategy';
+import googleStrategy from '@Lib/passport/strategy/googleStrategy';
 import kakaoStrategy from '@Lib/passport/strategy/kakaoStrategy';
 import localStrategy from '@Lib/passport/strategy/localStrategy';
 
@@ -23,5 +24,6 @@ passport.deserializeUser(async (user: UserInterface, done) => {
 passport.use(localStrategy);
 passport.use(githubStrategy);
 passport.use(kakaoStrategy);
+passport.use(googleStrategy);
 
 export default passport;
