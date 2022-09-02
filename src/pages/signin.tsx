903950e98c4aef5cba0c01586ad07555046a3a0c
MINJE-98 <jmj012100@gmail.com>
2022-09-02 21:04:41 +0900

    feat(component): google 로그인 추가

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -19,6 +19,7 @@ const SignIn = () => {
       </div>
       <SNSLoginButton strategy="github">깃허브로 로그인하기!</SNSLoginButton>
       <SNSLoginButton strategy="kakao">카카오로 로그인하기!</SNSLoginButton>
+      <SNSLoginButton strategy="google">구글로 로그인하기!</SNSLoginButton>
       <style jsx>{`
         .login {
           max-width: 21rem;
